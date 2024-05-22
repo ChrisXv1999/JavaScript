764c2f3cbbc281cb120eecc18d9a014c22ae3354
ChrisXv1999 <chrisxu0917@gmail.com>
2024-05-22 18:30:00 +0800

    nginx init

---
+++ ts/curry.ts
@@ -0,0 +1,11 @@
+interface CallOrConstruct {
+    (n?: number): string;
+    new (s: string): Date;
+  }
+  const fn:CallOrConstruct = (s:number|string):string|Date=>{
+    if(typeof s === 'number'){
+        return s.toString()
+    }else {
+        return new Date(s)
+    }
+  }
