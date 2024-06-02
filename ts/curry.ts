efdc224e41cacbe9376ea72c2f7102cf2d993c32
ChrisXv1999 <chrisxu0917@gmail.com>
2024-06-02 13:57:02 +0800

    mac init

--- ts/curry.ts
+++ ts/curry.ts
@@ -2,10 +2,10 @@ interface CallOrConstruct {
     (n?: number): string;
     new (s: string): Date;
   }
-  const fn:CallOrConstruct = (s:number|string):string|Date=>{
-    if(typeof s === 'number'){
-        return s.toString()
-    }else {
-        return new Date(s)
-    }
-  }
+  // const fn:CallOrConstruct = (s:number|string):string|Date=>{
+  //   if(typeof s === 'number'){
+  //       return s.toString()
+  //   }else {
+  //       return new Date(s)
+  //   }
+  // }
